1391f00216597b1cb86da5e1ee7e6869cf9e0ca4
mrlchapman <mrlchapman7@gmail.com>
2024-10-04 18:51:26 +0100

    commit

---
+++ arla.js
@@ -0,0 +1,17 @@
+// game.js
+
+let words = [
+    {word: 'cat', image: 'https://easy-peasy.ai/cdn-cgi/image/quality=80,format=auto,width=700/https://fdczvxmwwjwpwbeeqcth.supabase.co/storage/v1/object/public/images/dc48110e-ecc5-4b0d-81e0-2bd0bdb47cd1/fe679388-0783-4edb-aab5-ee12ac9413bd.png'},
+    {word: 'dog', image: 'https://cdn.meiker.io/assets/206891/2023/03/icon_2023030201221763fffa492e73c.png'},
+    {word: 'box', image: 'https://img.freepik.com/premium-vector/vector-cute-cardboard-box-cartoon-style_846317-1612.jpg'},
+    {word: 'bus', image: 'https://img.freepik.com/premium-vector/cartoon-school-bus-with-happy-students-inside-cartoon-vector-illustration_1080480-128128.jpg'},
+    {word: 'baby', image: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQGtP0UKg2ia4HEY46hB4L59-MQW9hVvFtmDQ&s'},
+    {word: 'car', image: 'https://toyez.pk/cdn/shop/products/Untitled-1_4e56d8e3-c1b5-493a-a454-f76168af5ebd.png?v=1615009568'},
+    {word: 'bed', image: 'https://i.etsystatic.com/41972288/r/il/b5e2d2/5863137677/il_570xN.5863137677_r7mu.jpg'},
+    {word: 'tap', image: 'https://png.pngtree.com/png-vector/20240316/ourlarge/pngtree-cartoon-dripping-faucet-png-image_11977886.png'},
+    {word: 'sat', image: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRnMP-Du_vT6uid4z_r4s4vCcKHjjBZBN-XcA&s'},
+    {word: 'hat', image: 'https://cdn.pixabay.com/photo/2022/11/20/20/53/hat-7605221_1280.png'},
+    {word: 'hop', image: 'https://images.template.net/82695/free-cartoon-hop-vector-oyq21.jpg'},
+    {word: 'mum', image: 'https://media.istockphoto.com/id/1141907474/vector/cute-young-mother-embracing-her-daughter-with-love.jpg?s=612x612&w=0&k=20&c=Wl4jUQPNmuvoZ_OHpDILbJPEYXp2Q2Qt7WCMmGXWRvY='},
+    {word: 'dad', image: 'https://www.shutterstock.com/image-vector/father-his-cute-little-daughter-600nw-1419842672.jpg'}
+];

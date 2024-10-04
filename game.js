f43806237b2187b79cb1ddc2bb0787ee4873689d
mrlchapman <mrlchapman7@gmail.com>
2024-10-04 10:39:09 +0100

    commit

---
+++ game.js
@@ -0,0 +1,20 @@
+// game.js
+
+let words = [
+    {word: 'cat', image: 'https://easy-peasy.ai/cdn-cgi/image/quality=80,format=auto,width=700/https://fdczvxmwwjwpwbeeqcth.supabase.co/storage/v1/object/public/images/dc48110e-ecc5-4b0d-81e0-2bd0bdb47cd1/fe679388-0783-4edb-aab5-ee12ac9413bd.png'},
+    {word: 'dog', image: 'https://cdn.meiker.io/assets/206891/2023/03/icon_2023030201221763fffa492e73c.png'},
+    {word: 'box', image: 'https://img.freepik.com/premium-vector/vector-cute-cardboard-box-cartoon-style_846317-1612.jpg'},
+    {word: 'bus', image: 'https://img.freepik.com/premium-vector/cartoon-school-bus-with-happy-students-inside-cartoon-vector-illustration_1080480-128128.jpg'},
+    {word: 'baby', image: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQGtP0UKg2ia4HEY46hB4L59-MQW9hVvFtmDQ&s'},
+    {word: 'berry', image: 'https://img.freepik.com/premium-photo/small-berries-cute-kawaii-cartoon-character_70898-7399.jpg'},
+    {word: 'tree', image: 'https://img.freepik.com/premium-photo/cartoon-tree-with-happy-face_889779-1034.jpg'},
+    {word: 'dish', image: 'https://static.vecteezy.com/system/resources/previews/026/825/898/original/dish-plate-white-cartoon-illustration-vector.jpg'},
+    {word: 'puppy', image: 'https://i.pinimg.com/736x/81/f6/11/81f611a78e99fe0e4d500e5fb2ee37d0.jpg'},
+    {word: 'car', image: 'https://toyez.pk/cdn/shop/products/Untitled-1_4e56d8e3-c1b5-493a-a454-f76168af5ebd.png?v=1615009568'},
+    // New words
+    {word: 'lunch', image: 'https://banner2.cleanpng.com/20180605/htg/kisspng-packed-lunch-school-meal-lunchbox-breakfast-food-delivery-5b171ce9a3ad70.7445370415282413856704.jpg'},
+    {word: 'bike', image: 'https://www.shutterstock.com/image-vector/happy-cute-kid-girl-riding-600nw-1555682591.jpg'},
+    {word: 'beach', image: 'https://i.pinimg.com/550x/da/4e/a9/da4ea9d88afb7a7fbbdde3b9f1fadc72.jpg'},
+    {word: 'church', image: 'https://static.vecteezy.com/system/resources/previews/034/812/322/non_2x/christian-church-colored-cartoon-illustration-free-vector.jpg'},
+    {word: 'flower', image: 'https://as2.ftcdn.net/v2/jpg/05/57/32/01/1000_F_557320121_KdMmeKo3hawtGPhNmfuIoZTUtHXCIit1.jpg'}
+];

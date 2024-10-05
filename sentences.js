8a4befc918c803094685ab70f682ab600b520e33
mrlchapman <mrlchapman7@gmail.com>
2024-10-05 13:10:56 +0100

    Update sentences.js

---
+++ sentences.js
@@ -0,0 +1,20 @@
+// sentences.js
+
+let sentences = [
+    {
+        image: 'images/tree.png',
+        words: ['The', 'tree', 'is', 'tall'],
+        correctOrder: ['The', 'tree', 'is', 'tall']
+    },
+    {
+        image: 'images/bike.png',
+        words: ['rides', 'bike', 'The', 'girl'],
+        correctOrder: ['The', 'girl', 'rides', 'bike']
+    },
+    {
+        image: 'images/tap.png',
+        words: ['is', 'on', 'The', 'tap'],
+        correctOrder: ['The', 'tap', 'is', 'on']
+    },
+    // Add more sentences as desired
+];

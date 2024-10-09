0b13941713c3d1457b696f0052f1721246ca91c1
mrlchapman <mrlchapman7@gmail.com>
2024-10-09 20:07:26 +0100

    commit

--- sentences.js
+++ sentences.js
@@ -2,19 +2,52 @@
 
 let sentences = [
     {
-        image: 'images/tree.png',
-        words: ['The', 'tree', 'is', 'tall'],
-        correctOrder: ['The', 'tree', 'is', 'tall']
+        image: 'https://img.freepik.com/premium-photo/cartoon-tree-with-happy-face_889779-1034.jpg',
+        words: ['The', 'tree', 'is', 'tall']
     },
     {
-        image: 'images/bike.png',
-        words: ['rides', 'bike', 'The', 'girl'],
-        correctOrder: ['The', 'girl', 'rides', 'bike']
+        image: 'images/bike.jpg',
+        words: ['Arla', 'rides', 'her', 'bike']
     },
     {
-        image: 'images/tap.png',
-        words: ['is', 'on', 'The', 'tap'],
-        correctOrder: ['The', 'tap', 'is', 'on']
+        image: 'https://png.pngtree.com/png-vector/20240316/ourlarge/pngtree-cartoon-dripping-faucet-png-image_11977886.png',
+        words: ['The', 'tap', 'is', 'on']
+    },
+    {
+        image: 'images/Beach.jpg',
+        words: ['Indie', 'goes', 'to', 'the', 'beach']
+    },
+    {
+        image: 'https://www.cakerstreet.com/upload/Product_images/fbImage/6th-birthday-cake-for-girls-25536-8d2c5be7b.JPG',
+        words: ['Indie', 'is', '6', 'years', 'old']
+    },
+    {
+        image: 'https://m.media-amazon.com/images/I/81H-bT4MJCL._AC_UF894,1000_QL80_.jpg',
+        words: ['Arla', 'is', '4', 'years', 'old']
+    },
+    {
+        image: 'https://www.shutterstock.com/image-vector/little-kid-wearing-doctor-costume-600nw-2203220743.jpg',
+        words: ['Mummy', 'is', 'a', 'nurse']
+    },
+    {
+        image: 'https://media.istockphoto.com/id/2024794209/vector/happy-young-physical-education-teacher-explaining-with-board.jpg?s=612x612&w=0&k=20&c=Px30VcDxQzzDe9JaThkgRJx0iWnX6Huq_PGCzgM7bIU=',
+        words: ['Daddy', 'is', 'a', 'teacher']
+    },
+    {
+        image: 'https://media.istockphoto.com/id/917332950/vector/vector-illustration-of-field-tree-and-sunrise-sky.jpg?s=612x612&w=0&k=20&c=NKn3LJoUatrNDprVbD1A947phrDcVap-5zQ0tNl73no=',
+        words: ['Today', 'is', 'sunny']
+    },
+    {
+        image: 'https://img.freepik.com/premium-photo/cartoon-girl-walking-beach-with-seagulls-flying-around-generative-ai_1034986-5634.jpg',
+        words: ['She', 'walked', 'by', 'the', 'sea']
+    },
+    {
+        image: 'https://img.freepik.com/premium-vector/two-boys-go-school-through-park-road_43633-6301.jpg?w=360',
+        words: ['We', 'went', 'to', 'the', 'park']
+    },
+    {
+        image: 'https://www.creativefabrica.com/wp-content/uploads/2023/05/28/Blue-cartoon-eyes-with-cute-eyelashes-K-Graphics-70762567-1-580x387.png',
+        words: ['Look', 'at', 'me']
     },
     // Add more sentences as desired
 ];
